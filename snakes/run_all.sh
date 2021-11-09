e1978901ca9e2c386d5cb197fe1171f04552d02c
tstannius <tobiasostannius@gmail.com>
2021-11-09 22:06:15 +0100

    Rename run script

---
+++ snakes/run_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+TARGET=all
+
+rm -rf tmp
+rm -rf out
+
+snakemake $TARGET \
+    -j1 -p \
+    --forceall \
+    --wms-monitor http://127.0.0.1:8000 \
+    --wms-monitor-arg workflow=NGS name=210101_MYRUN
