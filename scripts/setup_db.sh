bfb6273da9c6ac45ef65ad9cec1297d767adc5cd
tstannius <tobiasostannius@gmail.com>
2021-11-13 18:42:57 +0100

    Add db setup utility script

---
+++ scripts/setup_db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# ensure versions dir exists
+mkdir -p alembic/versions
+# remove any existing database and migrations
+rm -f app/snakemon.db; rm -f alembic/versions/*.py
+
+# make migrations
+# note the --autogenerate flag, which tells Alembic to use the alembic/env.py target_metadata
+alembic revision --autogenerate -m "Init db"
+
+# inspect migration script in alembic/versions and sanity check, before upgrade
+alembic upgrade head
