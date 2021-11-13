11aa785fd4ffb283b528e9bdbc959ace14d46610
tstannius <tobiasostannius@gmail.com>
2021-11-13 18:47:47 +0100

    Add async for db queries

---
+++ app/db/session.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm.session import sessionmaker
+
+SQLALCHEMY_DATABASE_URI = "sqlite+aiosqlite:///./snakemon.db" # TODO: get from settings
+# SQLALCHEMY_DATABASE_URI = "postgresql://user:password@postgresserver/db"
+
+async_engine = create_async_engine(SQLALCHEMY_DATABASE_URI, pool_pre_ping=True, connect_args={"check_same_thread": False})
+async_session = sessionmaker(bind=async_engine, autocommit=False, autoflush=False, expire_on_commit=False, class_=AsyncSession)
--- app/main.py
+++ app/main.py
@@ -1,23 +1,19 @@
 from fastapi import Depends, FastAPI, HTTPException, Request, status
 from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
-from sqlalchemy.orm import Session
 from pprint import pprint
 
 #-----------------------------------------------------------------------------#
 # db setup
 #-----------------------------------------------------------------------------#
-from db import crud, models, schemas
-from db.database import engine, SessionLocal
-
 # db dependency - TODO: put elsewhere
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+from db import crud, models, schemas
+from db.session import async_session
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession
 
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        yield session
 
 
 #-----------------------------------------------------------------------------#
@@ -32,17 +28,28 @@ async def service_info():
 
 
 @app.get("/create_workflow", status_code=status.HTTP_200_OK)
-async def create_workflow(workflow: str, name: str, request: Request, db: Session = Depends(get_db)):
+async def create_workflow(
+        workflow: str,
+        name: str,
+        request: Request,
+        session: AsyncSession = Depends(get_session)
+        ):
+    
     data = await request.form() # TODO: use metadata
     data = jsonable_encoder(data)
     print(data)
-    db_workflow = crud.create_workflow(db, workflow=schemas.WorkflowCreate(workflow=workflow, name=name))
+    
+    
+    db_workflow = await crud.create_workflow(session, workflow=schemas.WorkflowCreate(workflow=workflow, name=name))
     print(f"Creating workflow {db_workflow.workflow} with name {db_workflow.name}")
     return {"id": db_workflow.id}
 
 
 @app.post("/update_workflow_status", status_code=status.HTTP_200_OK)
-async def update_workflow_status(request: Request, db: Session = Depends(get_db)):
+async def update_workflow_status(
+        request: Request,
+        session: AsyncSession = Depends(get_session)
+        ):
     data = await request.form()
     data = jsonable_encoder(data)
     # db_workflow = crud.update_workflow(db, workflow=schemas.WorkflowUpdate(**da))
