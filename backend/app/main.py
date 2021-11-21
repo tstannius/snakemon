51723b52ca679d3c9e0cbaca5c2cc5ba6ae06773
tstannius <tobiasostannius@gmail.com>
2021-11-21 16:45:15 +0100

    Add CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import Depends, FastAPI, HTTPException, Request, status
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
 from pprint import pprint
 
 #-----------------------------------------------------------------------------#
@@ -20,6 +21,23 @@ async def get_session() -> AsyncGenerator[AsyncSession, None]:
 # api setup
 #-----------------------------------------------------------------------------#
 app = FastAPI()
+
+# origin: null - "... your server must read the value of the request's Origin 
+# header and use that value to set Access-Control-Allow-Origin"
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS/Errors/CORSMissingAllowOrigin
+origins = [
+    "http://localhost:5000", # js serve uses port 5000 as default
+    "http://localhost:3000", # react-scripts npm start uses port 3000 as default
+    "null", # use null if running client app through a static html file, i.e. no server
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET"], # could also be '*'
+    allow_headers=["*"],
+)
 
 
 @app.get("/api/service-info", status_code=status.HTTP_200_OK)
