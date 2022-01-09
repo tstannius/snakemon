07650cf717783fd3decfa4e430556f2e91af7103
tstannius <tobiasostannius@gmail.com>
2022-01-09 17:02:55 +0100

    Add frontend env

---
+++ frontend/src/env.ts
@@ -0,0 +1,13 @@
+// const env = process.env.APP_ENV;
+
+// let envApiUrl = '';
+
+// if (env === 'PROD') {
+//   envApiUrl = `https://${process.env.DOMAIN_PROD}`;
+// } else if (env === 'STAG') {
+//   envApiUrl = `https://${process.env.DOMAIN_STAG}`;
+// } else {
+//   envApiUrl = `http://${process.env.DOMAIN_DEV}`;
+// }
+
+export const apiUrl = "http://localhost:8000";
