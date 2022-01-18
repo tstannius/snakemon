ca7b842c08c5039bcd38490027b7451d76919907
tstannius <tobiasostannius@gmail.com>
2022-01-18 21:52:12 +0100

    Update allowed headers

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST"], # could also be '*'
-    allow_headers=["*"],
+    allow_headers=["Set-Cookie"],
 )
 
 app.include_router(api_router)
