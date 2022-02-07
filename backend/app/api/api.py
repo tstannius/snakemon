1d50c82e707bc3d1a4af51b27161e4f8fcf7e86c
tstannius <tobiasostannius@gmail.com>
2022-02-07 21:26:17 +0100

    Add smk prefix for snakemake endpoint

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -4,6 +4,6 @@ from app.api.endpoints import auth, jobs, smk, workflows
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(smk.router, tags=["snakemake"])
+api_router.include_router(smk.router, prefix="/smk", tags=["snakemake"])
 api_router.include_router(jobs.router, prefix="/jobs", tags=["jobs"])
 api_router.include_router(workflows.router, prefix="/workflows", tags=["workflows"])
--- snakes/run_all.sh
+++ snakes/run_all.sh
@@ -10,5 +10,5 @@ rm -rf out
 snakemake $TARGET \
     -j1 -p \
     --forceall \
-    --wms-monitor http://127.0.0.1:8000 \
+    --wms-monitor http://127.0.0.1:8000/smk \
     --wms-monitor-arg workflow=NGS name=210101_MYRUN
