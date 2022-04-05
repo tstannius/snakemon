953f3aa7eb2702f1b03f47a4cdfdefa542510025
tstannius <tobiasostannius@gmail.com>
2022-04-05 18:35:12 +0200

    Add workflow jobs stats endpoint

--- backend/app/api/endpoints/jobs.py
+++ backend/app/api/endpoints/jobs.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List, Optional
 
+from collections import Counter
+
 from app.db import crud, models, schemas
 from app.api import dependencies as deps
 
@@ -38,3 +40,43 @@ async def get_workflow_jobs(
     
     jobs = [j.json() for j in jobs]
     return jobs
+
+
+
+@router.get("/stats/")
+async def get_workflow_jobs_stats(
+            workflow_id: int,
+            session: AsyncSession = Depends(deps.get_session)):
+    """Get summary stats of jobs associated with workflow
+
+    Args:
+        workflow_id (int): id / primary key of workflow
+        session (AsyncSession, optional): Database session. Defaults to Depends(deps.get_session).
+
+    Raises:
+        HTTPException: Workflow or jobs not found
+
+    Returns:
+        List[schemas.Job]: Workflow jobs
+    """
+    jobs = await crud.read_job_stats(
+                session=session,
+                workflow_id=workflow_id)
+    
+    workflow = await crud.read_generic(session, workflow_id, models.Workflow)
+
+    if (not jobs) or (not workflow):
+        raise HTTPException(status_code=404, detail="Workflow or jobs not found")
+    
+    counts = dict(Counter(jobs))
+    
+    stats = {
+        "pending": (workflow.total - workflow.done - counts.get("Running", 0) - counts.get("Submitted", 0)), # submitted
+        "submitted": None, # TODO: add for cluster feature
+        "running": counts.get("Running", 0), # status is set as Running on creation
+        "error": counts.get("Error"), # TODO: add for cluster feature
+        "failed": None,
+        "succeeded": counts.get("Done")
+    }
+    
+    return stats
