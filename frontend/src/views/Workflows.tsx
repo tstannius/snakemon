a89d49845354e1b6967d1649497e0c5065a377e2
tstannius <tobiasostannius@gmail.com>
2022-04-15 16:42:15 +0200

    Translate to use hooks

--- frontend/src/views/Workflows.tsx
+++ frontend/src/views/Workflows.tsx
@@ -1,76 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Column } from 'react-table';
 import { Card } from 'react-bootstrap';
 import { WorkflowsTable } from "../components"
 
-interface IWorkflowsProps {}
-interface IWorkflowsState {
-  data: any[]
-}
-class Workflows extends React.Component<IWorkflowsProps, IWorkflowsState> {
-  constructor(props: IWorkflowsProps) {
-    super(props);
-    this.state = {
-      data: [],
-    };
-  }
 
-  // TODO:
-  // move elsewhere
-  // should ideally use React.useMemo(), find out how
-  getColumns(): Array<Column> {
-    const columns = [
-        {
-          Header: "ID",
-          accessor: "id"
-        },
-        {
-          Header: "Workflow",
-          accessor: "workflow"
-        },
-        {
-          Header: "Name",
-          accessor: "name"
-        },
-        {
-          Header: "Status",
-          accessor: "status"
-        },
-        {
-          Header: "Done",
-          accessor: "done"
-        },
-        {
-          Header: "Total",
-          accessor: "total"
-        },
-        {
-          Header: "Last update at",
-          accessor: "last_update_at"
-        },
-      ];
-    return(columns)
-  }
+export default function Workflows(): JSX.Element {
+  let [workflowsData, setWorkflowsData] = useState<Array<any>>([]);
 
-  componentDidMount() {
-    fetch("http://localhost:8000/workflows/", {method: "GET", mode: "cors"})
-    .then(response => response.json())
-    .then(data => {
-      this.setState({
-        data: data
+  useEffect(() => {
+    fetch("http://localhost:8000/workflows/", { method: "GET", mode: "cors" })
+      .then(response => response.json())
+      .then(data => {
+        setWorkflowsData(data);
       })
-    })
+  }, []) // The empty array ensures the useEffect is only run once
+
+  /**
+   * Return array of column objects for creating react table
+   * TODO:
+   * - should ideally use React.useMemo(), find out how
+   * - consider moving elsewhere
+   * 
+   * @returns {Array<Column>} Column headers and accessors for the table to be generated
+   */
+  function getColumns(): Array<Column> {
+    const columns = [
+      {
+        Header: "ID",
+        accessor: "id"
+      },
+      {
+        Header: "Workflow",
+        accessor: "workflow"
+      },
+      {
+        Header: "Name",
+        accessor: "name"
+      },
+      {
+        Header: "Status",
+        accessor: "status"
+      },
+      {
+        Header: "Done",
+        accessor: "done"
+      },
+      {
+        Header: "Total",
+        accessor: "total"
+      },
+      {
+        Header: "Last update at",
+        accessor: "last_update_at"
+      },
+    ];
+    return (columns)
   }
 
-  render(): JSX.Element {
-    return (
-      <Card body style={{margin: "3%"}}>
+  return (
+    <div id="WorkflowsTableContainer">
+      <Card body style={{ margin: "3%" }}>
         <Card.Body>
-          <WorkflowsTable columns={this.getColumns()} data={this.state.data}/>
+          <WorkflowsTable columns={getColumns()} data={workflowsData} />
         </Card.Body>
       </Card>
-      )
-    }
+    </div>
+  )
 }
-
-export default Workflows;
